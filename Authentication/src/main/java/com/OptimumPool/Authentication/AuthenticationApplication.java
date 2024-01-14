96a738311782d3cda66d1b40119e43aa5b4ddc64
Snehal.Kupekar <snehal.kupekar@itcinfotech.com>
2024-01-14 11:57:37 +0000

    gateway is not working

--- Authentication/src/main/java/com/OptimumPool/Authentication/AuthenticationApplication.java
+++ Authentication/src/main/java/com/OptimumPool/Authentication/AuthenticationApplication.java
@@ -7,9 +7,11 @@ import jakarta.servlet.FilterRegistration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AuthenticationApplication {
 
 	public static void main(String[] args) {
